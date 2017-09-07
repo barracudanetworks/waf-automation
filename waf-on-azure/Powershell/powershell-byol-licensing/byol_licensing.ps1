3ef65c1ba782a55075f01005a95e0967bb1fe7c4
root <root@puppet.cudase.local>
2017-09-07 14:10:24 +0530

    removed unwanted data

--- waf-on-azure/Powershell/powershell-byol-licensing/byol_licensing.ps1
+++ waf-on-azure/Powershell/powershell-byol-licensing/byol_licensing.ps1
@@ -1,7 +1,7 @@
 ﻿#Login-AzureRmAccount
 $value=(Get-Content .\test.txt -Raw) | ConvertFrom-Json | Select-Object -expand license1
 $postParams = @{token=$value; system_default_domain='cudasystem.local'}
-$publicip_output = Get-AzureRmVM -ResourceGroupName 'hiscox-test' -Name 'hiscox-test' | Get-AzureRmPublicIpAddress -Name 'hiscox-test-ip'
+$publicip_output = Get-AzureRmVM -ResourceGroupName 'test' -Name 'testing' | Get-AzureRmPublicIpAddress -Name 'test-ip'
 $ipadd = $publicip_output|Select-Object -ExpandProperty IpAddress
 echo $ipadd
 $url = "http://$ipadd`:8000"
@@ -12,7 +12,7 @@ $eula = Invoke-WebRequest $url/ -Method GET
 echo "$eula"
 if ($eula -like '*You Must Accept the Barracuda Product Terms Below to Configure Your Barracuda Virtual Appliance*')
 {
-$eulasign = @{name_sign=<name>; email_sign="<email-name>"; company_sign="Hiscox"; eula_hash_val=ed4480205f84cde3e6bdce0c987348d1d90de9db; action=save_signed_eula}
+$eulasign = @{name_sign=<name>; email_sign="<email-name>"; company_sign="company-name"; eula_hash_val=ed4480205f84cde3e6bdce0c987348d1d90de9db; action=save_signed_eula}
 Invoke-WebRequest $url/ -Method POST -Body $eulasign
 }
 else
