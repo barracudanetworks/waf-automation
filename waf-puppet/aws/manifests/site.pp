063ba80e84ac0f9fc301645a6a8274f470f4b5c9
root <root@puppet.cudase.local>
2017-09-25 19:11:10 +0530

    documentation update

--- waf-puppet/aws/manifests/site.pp
+++ waf-puppet/aws/manifests/site.pp
@@ -1,4 +1,7 @@
-# This is the main manifest file in the environment. A puppet agent node connecting to this environment will be rendered a catalog based on this manifest. In this particular manifest, roles are assigned based on the node's hostname. For example, the host with the hostname of 'awsserver', will be executing the server.pp manifest in the roles module. 
+# This is the main manifest file in the environment. 
+#A puppet agent node connecting to this environment will be rendered a catalog based on this manifest. 
+#In this particular manifest, roles are assigned based on the node's hostname. 
+#For example, the host with the hostname of 'awsserver', will be executing the server.pp manifest in the roles module. 
 node 'awsnode' {
 include roles
 }
