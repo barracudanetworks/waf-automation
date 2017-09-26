4ddfa0f30b39533699f71df5b7d7adb75b9c933a
root <root@puppet.cudase.local>
2017-09-26 11:14:32 +0530

    common.yaml updated

--- waf-puppet/aws/modules/profiles/manifests/base.pp
+++
@@ -1,2 +0,0 @@
-class profiles::base {
-}
--- waf-puppet/aws/modules/roles/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-class roles {
-#	include profiles::base
-#	include profiles::wafsecgroup
-#	include profiles::serversecgroup
-#	include aws
-#	include awscudawafconfig::config
-#	include vrsinaws::vrsconfig
-}
-
