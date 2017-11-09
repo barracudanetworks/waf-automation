c858847b27cfe000613e3bbb687443017632c6a6
root <root@puppet.cudase.local>
2017-11-09 11:12:10 +0530

    manifest level change

--- hands_on_lab_env/stagingaws/modules/profiles/manifests/wafconfig.pp
+++ hands_on_lab_env/stagingaws/modules/profiles/manifests/wafconfig.pp
@@ -1,11 +1,13 @@
 #This manifest will create the service on the WAF
 class profiles::wafconfig {
+$vip = $waf_private_ip
+
 wafservices  { 'WAFSVC-1':
   ensure	=> present,
   name          => 'WAFSERVICE',
   type		=> 'http',
   mask		=> '255.255.255.255',
-  ip_address	=> '3.3.3.3',
+  ip_address	=> '$vip',
   port		=> '80',
   group		=> 'default',
   vsite		=> 'default',
