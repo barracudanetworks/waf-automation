951f3638115ea23819010901162670c2e00e19de
bwolmarans <itsbrett@gmail.com>
2021-03-17 08:55:01 -0700

    updated for python2 compatibility
    also added app id to the display

--- waf-as-a-service-api/waas_rest_api_example.py
+++ waf-as-a-service-api/waas_rest_api_example.py
@@ -2,7 +2,12 @@ import requests
 import pprint
 import sys
 from getpass import getpass
-from urllib.parse import urlencode, urljoin
+try:
+    from urllib.parse import urlparse
+    from urllib.parse import urljoin
+except ImportError:
+    from urlparse import urlparse
+    from urlparse import urljoin
 
 API_BASE = "https://api.waas.barracudanetworks.com/v1/waasapi/"
 
@@ -13,7 +18,7 @@ def waas_api_login(email, password):
     return response_json['key']
 
 def waas_api_get(token, path):
-    res = requests.get(urljoin(API_BASE, path), headers={'auth-api': token})
+    res = requests.get(urljoin(API_BASE, path), headers={"Content-Type": "application/json", 'auth-api': token})
     res.raise_for_status()
     return res.json()
 
@@ -22,13 +27,14 @@ if __name__ == '__main__':
         email = sys.argv[1]
         password = sys.argv[2]
     else:
-        email = input("Enter user email:")
+        email = raw_input("Enter user email:")
         password = getpass("Enter user password:")
     token = waas_api_login(email, password)
 
-    # Show list of applications, and servers for each application
-    apps = waas_api_get(token, 'applications')
-    for app in apps['results']:
-        print("Application: {}".format(app['name']))
-        for server in app['servers']:
-            print("\tServer: {} ({} {}:{})".format(server['name'], server['protocol'], server['host'], server['port']))
+# Show list of applications, and servers for each application
+apps = waas_api_get(token, 'applications')
+for app in apps['results']:
+    #print(app['id'])
+    print("Application Name/id: {} {}".format(app['name'], app['id']))
+    for server in app['servers']:
+        print("\tServer: {} ({} {}:{})".format(server['name'], server['protocol'], server['host'], server['port']))
